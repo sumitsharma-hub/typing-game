be741e94c96bda72be2f1afacacb61c2f3190e91
Amogh Madan <amoghmadaan@gmail.com>
2023-07-09 16:13:05 +0530

    Fix port.

--- typing_game_api/src/settings.ts
+++ typing_game_api/src/settings.ts
@@ -6,4 +6,4 @@ dotenv.config()
 
 export const BASE_DIR: string = path.dirname(path.dirname(__filename))
 
-export const MONGO_URI: string = process.env.MONGO_URI ?? 'mongodb://127.0.0.1:8000/typinggame'
+export const MONGO_URI: string = process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/typinggame'
