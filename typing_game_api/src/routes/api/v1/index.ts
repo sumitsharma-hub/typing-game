4bbe05eb2c9264697e8b6a3b7aab6c20ffeb9b7b
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-14 16:20:49 +0530

    added google login route.

--- typing_game_api/src/routes/api/v1/index.ts
+++ typing_game_api/src/routes/api/v1/index.ts
@@ -1,16 +1,22 @@
-import { Router } from 'express'
+import { Router } from "express";
 
-import accounts from './accounts'
-import generator from './text-generator'
+import accounts from "./accounts";
+import generator from "./text-generator";
+import googleLogin from "./google-login";
+
+
+
+
 
 const urlpatterns: Map<string, Router> = new Map<string, Router>([
-  ['/accounts', accounts],
-  ['/generate', generator]
-])
+  ["/accounts", accounts],
+  ["/generate", generator],
+  ["/api", googleLogin],
+]);
 
-const v1 = Router()
+const v1 = Router();
 urlpatterns.forEach((router: Router, prefix: string) => {
-  v1.use(prefix, router)
-})
+  v1.use(prefix, router);
+});
 
-export default v1
+export default v1;
