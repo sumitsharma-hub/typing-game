fefab80e80d6d724e5ad915cee990f832170b43c
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-14 16:21:39 +0530

    Added login register route.

--- typing_game_webui/src/constants/browser.ts
+++ typing_game_webui/src/constants/browser.ts
@@ -1,6 +1,9 @@
-const Browser={
-    ROOT: '/',
-    RANDOM:'/game',
-    HTTP_404:'/404'
-}
+const Browser = {
+  ROOT: "/",
+  RANDOM: "/game",
+  HTTP_404: "/404",
+  ASTERISK: "*",
+  LOGIN:"/login",
+  REGISTER:"register"
+};
 export default Browser;
