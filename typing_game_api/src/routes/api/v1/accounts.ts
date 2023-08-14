af81e13c90cc746042592ddc90f710d8a6fc93ef
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-14 16:20:25 +0530

    Added register route.

--- typing_game_api/src/routes/api/v1/accounts.ts
+++ typing_game_api/src/routes/api/v1/accounts.ts
@@ -1,12 +1,16 @@
-import { Router } from 'express'
+import { Router } from "express";
 
-import { AccountController } from '../../../controllers'
+import { AccountController } from "../../../controllers";
+import SessionController from "../../../controllers/session";
 
-const accountController = new AccountController()
+const accountController = new AccountController();
+const sessionController=new SessionController
 
-const accounts = Router()
-accounts.route('/login').post(accountController.login)
-accounts.route('/detail').get(accountController.detail)
-accounts.route('/logout').delete(accountController.logout)
+const accounts = Router();
+accounts.route("/login").post(accountController.login);
+accounts.route("/register").post(accountController.register);
+accounts.route("/detail").get(accountController.detail);
+accounts.route("/logout").delete(accountController.logout);
+// accounts.route("/api/sessions/oauth/google").get(accountController.googleOauthHandler);
 
-export default accounts
+export default accounts;
