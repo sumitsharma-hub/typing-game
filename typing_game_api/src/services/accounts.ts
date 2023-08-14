81323c04f324d8b88d8da6093bf9b5487785f42d
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-14 16:21:09 +0530

    Refactored the code.

--- typing_game_api/src/services/accounts.ts
+++ typing_game_api/src/services/accounts.ts
@@ -1,37 +1,52 @@
-import { type ILogin } from '../interfaces/login'
-import { type ITokenDetail, type IUser, type IUserDetail } from '../interfaces/user'
-import { User } from '../models'
-import { generateKey } from '../utils/token'
+import { IRegister, type ILogin } from "../interfaces/login";
+import { type ITokenDetail, type IUser, type IUserDetail } from "../interfaces/user";
+import { User } from "../models";
+import { generateKey } from "../utils/token";
 
 export default class AccountService {
-  async performLogin (payload: ILogin): Promise<ITokenDetail | null> {
-    const user: IUser | null = await User.findOne({ email: payload.email, isActive: true })
-    if (user == null) return null
-    const match: boolean = await user.validatePassword(payload.password)
-    if (!match) return null
+  async performLogin(payload: ILogin): Promise<ITokenDetail | null> {
+    const user: IUser | null = await User.findOne({ email: payload.email, isActive: true });
+    if (user == null) return null;
+    const match: boolean = await user.validatePassword(payload.password);
+    if (!match) return null;
     if (user.token == null) {
-      user.token = { key: generateKey() }
-      user.lastLogin = new Date()
-      await user.save()
+      user.token = { key: generateKey() };
+      user.lastLogin = new Date();
+      await user.save();
     }
 
-    return { token: user.token.key }
+    return { token: user.token.key };
   }
 
-  async retrieveDetails (user: IUser): Promise<IUserDetail> {
+  async retrieveDetails(user: IUser): Promise<IUserDetail> {
     const detail = {
       email: user.email,
       firstName: user.firstName,
       lastName: user.lastName,
       dateJoined: user.dateJoined,
-      isAdmin: user.isAdmin
-    }
-    return detail
+      isAdmin: user.isAdmin,
+    };
+    return detail;
   }
 
-  async performLogout (user: IUser): Promise<Record<string, unknown>> {
-    user.set('token', undefined, { strict: false })
-    await user.save()
-    return {}
+  async performLogout(user: IUser): Promise<Record<string, unknown>> {
+    
+    user.set("token", undefined, { strict: false });
+    await user.save();
+    return {};
   }
+
+  async performRegisteration(payload: IRegister) {
+    const user: IRegister | null = await User.findOne({ email: payload.email });
+    if (user) return null;
+    const newUser = new User(payload);
+    console.log(newUser)
+    if (newUser.token == null) {
+      newUser.token = { key: generateKey() };
+      newUser.lastLogin = new Date();
+      await newUser.save();
+    }
+    await newUser.save();
+    return newUser;
+  } 
 }
