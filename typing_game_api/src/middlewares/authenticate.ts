f9569f07f7cda55981b6e993c13175cf9f3db7be
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-14 16:23:48 +0530

    refactored the code.

--- typing_game_api/src/middlewares/authenticate.ts
+++ typing_game_api/src/middlewares/authenticate.ts
@@ -1,45 +1,45 @@
-import { type Request, type Response, type NextFunction } from 'express'
+import { type Request, type Response, type NextFunction } from "express";
 
-import { User } from '../models'
-import { type IUser } from '../interfaces/user'
+import { User } from "../models";
+import { type IUser } from "../interfaces/user";
 
-const EXCLUDE_URLS = ['/api/v1/accounts/login']
+const EXCLUDE_URLS = ["/api/v1/accounts/login", "/api/v1/accounts/register", "/api/v1/api/sessions/oauth/google", "/api/v1/generate/text"];
 
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser
+      user?: IUser;
     }
   }
 }
 
-export default async function authenticate (
+export default async function authenticate(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<Response | undefined> {
   if (EXCLUDE_URLS.includes(request.path)) {
-    next()
-    return
+    next();
+    return;
   }
-  const keyword = 'Token'
-  const authorization: string | undefined = request.headers?.authorization
+  const keyword = "Token";
+  const authorization: string | undefined = request.headers?.authorization;
   if (authorization === undefined) {
-    return response.status(401).json({ detail: 'Unauthorized' })
+    return response.status(401).json({ detail: "Unauthorized" });
   }
-  const values = authorization.split(' ')
+  const values = authorization.split(" ");
   if (values.length !== 2) {
-    return response.status(401).json({ detail: 'Unauthorized' })
+    return response.status(401).json({ detail: "Unauthorized" });
   }
   if (keyword.toLowerCase() !== values[0].toLowerCase()) {
-    return response.status(401).json({ detail: 'Unauthorized' })
+    return response.status(401).json({ detail: "Unauthorized" });
   }
   const user: IUser | null = await User.findOne({
-    'token.key': values[1]
-  })
-  if (user == null) {
-    return response.status(403).json({ detail: 'Forbidden' })
+    "token.key": values[1],
+  });
+  if (user === null) {
+    return response.status(403).json({ detail: "Forbidden" });
   }
-  request.user = user
-  next()
+  request.user = user;
+  next();
 }
