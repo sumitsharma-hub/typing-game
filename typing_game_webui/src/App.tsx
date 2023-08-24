33c65b67b34f2348fc994a2d8505f0caeb45fb0d
Sumit Sharma <sumit.sharma5099@gmail.com>
2023-08-24 15:05:12 +0530

    added RTK provider.

--- typing_game_webui/src/App.tsx
+++ typing_game_webui/src/App.tsx
@@ -1,24 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import { Browser } from "./constants";
-import { Home, Random, HTTP404 ,Login, Register} from "./pages";
+import { Home, Random, HTTP404, Login, Register } from "./pages";
+import store from "./store/store";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path={Browser.LOGIN} element={<Login />}></Route>
-        <Route path={Browser.REGISTER} element={<Register />}></Route>
-        <Route path={Browser.ROOT} element={<Home />}></Route>
-        <Route path={Browser.RANDOM}>
-          <Route path={"random"} element={<Random />} />
-          <Route path={"regular"} element={<Random />} />
-        </Route>
-        <Route path={Browser.HTTP_404} element={<HTTP404 />}></Route>
-        <Route path={Browser.ASTERISK} element={<HTTP404 />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={Browser.LOGIN} element={<Login />}></Route>
+          <Route path={Browser.REGISTER} element={<Register />}></Route>
+          <Route path={Browser.ROOT} element={<Home />}></Route>
+          <Route path={Browser.RANDOM}>
+            <Route path={"random"} element={<Random />} />
+            <Route path={"regular"} element={<Random />} />
+          </Route>
+          <Route path={Browser.HTTP_404} element={<HTTP404 />}></Route>
+          <Route path={Browser.ASTERISK} element={<HTTP404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
